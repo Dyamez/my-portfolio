b6c7075a6fb319321f80208b9d74e5610cf2b7ae
Dyamez <dyamez@outlook.com>
2023-08-09 14:58:09 -0700

    Added components
    resume & header

---
+++ src/components/Resume.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class Resume extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="resume">
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
+        </div>
+
+        <div className="nine columns main-col">
+          {resumeData.education &&
+            resumeData.education.map((item) => {
+              return (
+                <div className="row item">
+                  <div className="twelve columns">
+                    <h3>{item.UniversityName}</h3>
+                    <p className="degree">{item.Degree}</p>
+                    <em className="date">{item.YearOfPassing}</em>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,33 @@
+let resume = {
+  name: "Edward James Manansala",
+  role: "Software Developer Student",
+  socialLinks: [
+    {
+      name: "facebook",
+      url: "https://www.facebook.com/edward.james.manansala/",
+      className: "fa fa-facebook",
+    },
+    {
+      name: "github",
+      url: "https://github.com/Dyamez",
+      className: "fa fa-github",
+    },
+    {
+      name: "discord",
+      userName: "dyamez",
+      className: "fa fa-discord",
+    },
+  ],
+  aboutme: "Currently a student at ThriveDX taking up Software Development",
+  address: "Las Vegas, NV",
+  education: [
+    {
+      UniversityName:
+        "University of the City of Manila (Pamantasan ng Lungsod ng Maynila)",
+      Degree: "Bachelor of Arts in Mass Communication ",
+      YearOfPassing: "2001",
+    },
+  ],
+};
+
+export default resume;
